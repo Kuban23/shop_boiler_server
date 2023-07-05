342e10e7fc4f2fd26fc850389c604f04e631b853
Esin <kubanesin@gmail.com>
2023-07-05 14:39:27 +0300

    feat: создал роут boiler-parts для пагинции

--- src/boiler_parts/boiler_parts.controller.ts
+++ src/boiler_parts/boiler_parts.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+
+import { BoilerPartsService } from './boiler_parts.service';
+import { AuthenticatedGuard } from 'src/auth/authenticated.guard';
 
 @Controller('boiler-parts')
-export class BoilerPartsController {}
+export class BoilerPartsController {
+  constructor(private readonly boilerPartsService: BoilerPartsService) {}
+
+  // получаю элементы boiler-parts
+  @Get()
+  @UseGuards(AuthenticatedGuard)
+  paginateAndFilter(@Query() query) {
+    return this.boilerPartsService.paginateAndFilter(query);
+  }
+}
--- src/boiler_parts/boiler_parts.service.ts
+++ src/boiler_parts/boiler_parts.service.ts
@@ -1,4 +1,64 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Op } from 'sequelize';
+
+import { BoilerParts } from './boiler-parts.model';
+import { IBoilerPartsQuery } from './types';
 
 @Injectable()
-export class BoilerPartsService {}
+export class BoilerPartsService {
+  // получаю доступ к таблице продуктов
+  constructor(
+    @InjectModel(BoilerParts)
+    private boilerPartsModel: typeof BoilerParts,
+  ) {}
+
+  // метод получения товаров, будет возвращать постраничную пагинцию и возвращать элементы по опред. фильтру
+  async paginateAndFilter(
+    query: IBoilerPartsQuery,
+  ): Promise<{ count: number; rows: BoilerParts[] }> {
+    const limit = +query.limit;
+    const offset = +query.offset * 20;
+    return this.boilerPartsModel.findAndCountAll({
+      limit,
+      offset,
+    });
+  }
+  // нахожу бестцеллеры
+  async bestsellers(): Promise<{ count: number; rows: BoilerParts[] }> {
+    return this.boilerPartsModel.findAndCountAll({
+      where: { bestseller: true },
+    });
+  }
+
+  // нахожу новые товары
+  async new(): Promise<{ count: number; rows: BoilerParts[] }> {
+    return this.boilerPartsModel.findAndCountAll({
+      where: { new: true },
+    });
+  }
+
+  // нахожу один товар по сайту
+  async findOne(id: number | string): Promise<BoilerParts> {
+    return this.boilerPartsModel.findOne({
+      where: { id },
+    });
+  }
+
+  // нахожу товар по имени
+  async findOneByName(name: string): Promise<BoilerParts> {
+    return this.boilerPartsModel.findOne({
+      where: { name },
+    });
+  }
+
+  // получаю список товаров подходящих по определенной строчке
+  async searchByString(
+    str: string,
+  ): Promise<{ count: number; rows: BoilerParts[] }> {
+    return this.boilerPartsModel.findAndCountAll({
+      limit: 20,
+      where: { name: { [Op.like]: `%${str}%` } },
+    });
+  }
+}
