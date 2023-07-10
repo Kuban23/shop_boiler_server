7bf846547b28688dfe64c49d9586e715c238a03d
Esin <kubanesin@gmail.com>
2023-07-10 11:41:19 +0300

    feat: создал документацию payment для swagger

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,14 +1,17 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { ApiOkResponse } from '@nestjs/swagger';
 
 import { PaymentService } from './payment.service';
 import { MakePaymentDto } from './dto/make-paymen.dto';
 import { AuthenticatedGuard } from 'src/auth/authenticated.guard';
+import { MakePaymentResponse } from './types';
 
 @Controller('payment')
 export class PaymentController {
   constructor(private paymentService: PaymentService) {}
 
   // Запрос на оплату
+  @ApiOkResponse({ type: MakePaymentResponse })
   @UseGuards(AuthenticatedGuard)
   @Post()
   makePayment(@Body() makePaymentDto: MakePaymentDto) {
